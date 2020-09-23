84f8226473343cd56fdd6b702c3638f0b76219d4
dr4co-co <jona.paulp@gmx.de>
2020-09-23 22:28:15 +0200

    relative path

--- Ui/ConsoleUi.py
+++ Ui/ConsoleUi.py
@@ -1,7 +1,12 @@
 from RiotApi.LeagueOfLegendsApiConnection import LeagueOfLegendsApiConnection
+import os
+
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
 
 def Main():
-    with open('pfad', 'r') as file:
+    with open('../assets/api_key.txt', 'r') as file:
         apiKey = file.read()
 
     api = LeagueOfLegendsApiConnection(apiKey)
