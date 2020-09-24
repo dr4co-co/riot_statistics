27d691aeca5352956e079b827be1af20c00bab33
dr4co-co <jona.paulp@gmx.de>
2020-09-24 16:05:33 +0200

    champion analyzer

---
+++ Logic/ChampionAnalyzer.py
@@ -0,0 +1,5 @@
+from RiotApi.LeagueOfLegendsApiConnection import LeagueOfLegendsApiConnection
+
+class ChampionAnalyzer():
+    def __init__(self, api_key):
+        api = LeagueOfLegendsApiConnection(api_key)
